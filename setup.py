c59ae47e1e925e735be7283b525b172c196063d5
Masaki Kozuki <masaki-kozuki@g.ecc.u-tokyo.ac.jp>
2019-05-19 16:43:25 +0900

    add minimal setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="fastai_audio",
+    version='1.0.51',
+    url='https://github.com/maksak/fastai_audio.git',
+    author='Masaki Kozuki',
+    author_email='masaki-kozuki@g.ecc.u-tokyo.ac.jp',
+    description='fastai audio',
+    packages=find_packages("fastai_audio"),
+)
